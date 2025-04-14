5b74bcd389e1748f3572d14f847f971dd587ed01
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-04-14 16:11:30 +0200

    points

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
+#![feature(slice_from_ptr_range)] // shader
 #![allow(non_snake_case)] 
-//use ui::{Error, Result, throws};
-pub type Error = Box<dyn core::error::Error>;
-pub type Result<T=(),E=Error> = core::result::Result<T, E>;
-use fehler::throws;
+use ui::{Error, Result, throws};
 
 use owning_ref::OwningRef;
 type Array<T=f32> = OwningRef<Box<memmap::Mmap>, [T]>;
@@ -10,71 +8,19 @@ type Array<T=f32> = OwningRef<Box<memmap::Mmap>, [T]>;
     OwningRef::new(Box::new(unsafe{memmap::Mmap::map(&std::fs::File::open(path)?)}?)).map(|data| bytemuck::cast_slice(&*data))
 }
 
-mod terrain; //use terrain::Terrain;
+mod terrain; use terrain::Terrain;
 
-//use ui::{Result, time};
-use vector::{xyz, MinMax, vec3};//xy, size, int2, vec2, xyz, vec3, rotate, xyzw, vec4, mat4};
-//use ui::vulkan::{Context, Commands, Arc, ImageView, Image, default, ImageCreateInfo, Format, ImageUsage};
+use vector::{xyz, MinMax, vec3, xy, size, int2, vec2, rotate, xyzw, vec4, mat4};
+use ui::vulkan::{Context, Commands, Arc, ImageView, Image, default, ImageCreateInfo, Format, ImageUsage};
 
 struct App {
-	//terrain: Terrain,
-	//view_position: vec2,
-	//yaw: f32,
+	terrain: Terrain,
+	view_position: vec2,
+	yaw: f32,
 }
 
 impl App {
-	/*fn new(context: &Context, commands: &mut Commands) -> Result<Self> {
-	Ok(Self{
-	terrain: Terrain::new(context, commands, ground, water, meters_per_pixel, z, color)?,
-	//trees: Trees::new(context, /*ground*/water, meters_to_normalized, z)?,
-	view_position: xy{x: 0., y: 0.}, yaw: 0.
-	})
-	}*/
-}
-
-/*impl ui::Widget for App {
-fn paint(&mut self, context@Context{memory_allocator, ..}: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result<()> {
-let Self{terrain, /*view_position, yaw*/} = self;
-/*let image_size = {let [x,y,_] = target.image().extent(); xy{x,y}};
-let aspect_ratio = image_size.x as f32/image_size.y as f32;
-
-let view_projection = |xyz{x,y,z}:vec3| {
-let xy{x,y} = rotate(*yaw, xy{x,y} - *view_position);
-let xy{x: y, y: z} = rotate(-std::f32::consts::PI/3., xy{x: y, y: z});
-let z = (z-1.)/2.;
-let n = 1./4.;
-let f = 1.;
-if true { xyzw{x, y: aspect_ratio*y, z: -z, w: 1.}  } else {
-let zz = -f/(f-n);
-let z1 = -(f*n)/(f-n);
-xyzw{x, y: aspect_ratio*y, z: zz*z+z1, w: -z}
-}
-};
-fn from_linear(linear: impl Fn(vec3)->vec4) -> mat4 {
-let w = linear(xyz{x:0.,y:0.,z:0.});
-let xyz{x,y,z} = xyz{x: xyz{x:1.,y:0.,z:0.}, y:xyz{x:0.,y:1.,z:0.}, z: xyz{x:0.,y:0.,z:1.}}.map(|e| linear(e)-w);
-xyzw{x,y,z,w}
-}
-let view_projection = from_linear(view_projection);
-
-let depth = ImageView::new_default(Image::new(memory_allocator.clone(), ImageCreateInfo{
-format: Format::D16_UNORM,
-extent: target.image().extent(),
-usage: ImageUsage::DEPTH_STENCIL_ATTACHMENT,
-..default()
-}, default())?)?;
-
-terrain.render(context, commands, target.clone(), depth.clone(), view_projection)?;*/
-
-// *yaw += std::f32::consts::PI/6./60.;
-Ok(())
-}
-fn event(&mut self, _size: size, _context: &mut ui::EventContext, _event: &ui::Event) -> Result<bool> { Ok(true/*Keep repainting*/) }
-}*/
-
-//fn main() -> Result { ui::run("lidar", Box::new(move |context, commands| Ok(Box::new(time("init", || App::new(context, commands))?)))) }
-
-fn main() -> Result {
+fn new(context: &Context, commands: &mut Commands) -> Result<Self> {
 	let name = "2684500_1248500";
 	let ref cache = format!("{}/.cache/lidar/{name}", std::env::var("HOME")?);
 	if !std::fs::exists(cache)? {
@@ -97,6 +43,52 @@ fn main() -> Result {
 		std::fs::write(cache, bytemuck::cast_slice::<vec3,_>(&points))?;
 		println!("{cache}");
 	}
-	dbg!(map::<vec3>(cache));
+	let ref points = map::<vec3>(cache)?;
+	Ok(Self{
+	terrain: Terrain::new(context, commands, points)?,
+	view_position: xy{x: 0., y: 0.}, yaw: 0.
+	})
+}}
+
+impl ui::Widget for App {
+fn paint(&mut self, context@Context{memory_allocator, ..}: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result<()> {
+	let Self{terrain, view_position, yaw} = self;
+	let image_size = {let [x,y,_] = target.image().extent(); xy{x,y}};
+	let aspect_ratio = image_size.x as f32/image_size.y as f32;
+	
+	let view_projection = |xyz{x,y,z}:vec3| {
+	let xy{x,y} = rotate(*yaw, xy{x,y} - *view_position);
+	let xy{x: y, y: z} = rotate(-std::f32::consts::PI/3., xy{x: y, y: z});
+	let z = (z-1.)/2.;
+	let n = 1./4.;
+	let f = 1.;
+	if true { xyzw{x, y: aspect_ratio*y, z: -z, w: 1.}  } else {
+	let zz = -f/(f-n);
+	let z1 = -(f*n)/(f-n);
+	xyzw{x, y: aspect_ratio*y, z: zz*z+z1, w: -z}
+	}
+	};
+	fn from_linear(linear: impl Fn(vec3)->vec4) -> mat4 {
+	let w = linear(xyz{x:0.,y:0.,z:0.});
+	let xyz{x,y,z} = xyz{x: xyz{x:1.,y:0.,z:0.}, y:xyz{x:0.,y:1.,z:0.}, z: xyz{x:0.,y:0.,z:1.}}.map(|e| linear(e)-w);
+	xyzw{x,y,z,w}
+	}
+	let view_projection = from_linear(view_projection);
+	
+	let depth = ImageView::new_default(Image::new(memory_allocator.clone(), ImageCreateInfo{
+	format: Format::D16_UNORM,
+	extent: target.image().extent(),
+	usage: ImageUsage::DEPTH_STENCIL_ATTACHMENT,
+	..default()
+	}, default())?)?;
+	
+	terrain.render(context, commands, target.clone(), depth.clone(), view_projection)?;
+	
+	// *yaw += std::f32::consts::PI/6./60.;
 	Ok(())
 }
+//fn event(&mut self, _size: size, _context: &mut ui::EventContext, _event: &ui::Event) -> Result<bool> { Ok(false/*true*//*Keep repainting*/) }
+}
+
+fn main() -> Result { ui::run("lidar", Box::new(move |context, commands| Ok(Box::new(App::new(context, commands)?)))) }
+
--- src/terrain.rs
+++ src/terrain.rs
@@ -1,92 +1,29 @@
-//use ui::{vulkan, shader};
-//shader!{terrain} // z: f32, NdotL: f32, water: f32,
-use vector::vec3;
-struct Vertex {
-	position: vec3/*<f32>*/,
-}
-//use vulkan::{Arc, Subbuffer, Image as GPUImage};
+use ui::{vulkan, shader};
+shader!{terrain}
+use vulkan::Subbuffer;
 pub struct Terrain {
-	//pass: terrain::Pass,
-	/*vertex_grid_size_x: u32,
-	grid: Subbuffer::<[u32]>,
+	pass: terrain::Pass,
 	vertices: Subbuffer::<[terrain::Vertex]>,
-	image: Arc<GPUImage>,*/
 }
 
-/*use {ui::Result, std::cmp::min, num::sq, vector::{xy, xyz, dot, normalize, cross, mat4}, image::{Image, rgba8}};
-use vulkan::{Context, buffer, BufferUsage, Commands, ImageView, image, WriteDescriptorSet, linear};*/
+use {ui::Result, vector::{vec3, mat4}};
+use vulkan::{Context, from_iter, BufferUsage, Commands, Arc, ImageView, PrimitiveTopology};
 impl Terrain {
-	/*pub fn new(context: &Context, commands: &mut Commands, ground: &Image<impl AsRef<[f32]>>, water: &Image<impl AsRef<[f32]>>, meters_per_pixel: f32, z: impl Fn(f32)->f32, color: &Image<impl AsRef<[rgba8]>>) -> Result<Self> {
-		let [ground, water] = [ground.as_ref(), water.as_ref()];
-		let size = ground.size;
-		let vertex_grid_size_x = {assert_eq!(size.x, size.y); size.x};
-		let vertex_stride = vertex_grid_size_x;
-		let vertices = buffer(context, BufferUsage::VERTEX_BUFFER, ground.data.len())?;
-		{
-			let mut vertices = vertices.write()?;
-			for y in 1..size.y-1 { for x in 1..size.x-1 {
-				let dx_z = (water[xy{x: x+1, y}]-water[xy{x: x-1, y}])/(2.*meters_per_pixel);
-				let dy_z = (water[xy{x, y: y+1}]-water[xy{x, y: y-1}])/(2.*meters_per_pixel);
-				let n = normalize(cross(xyz{x: 1., y: 0., z: dx_z}, xyz{x: 0., y: 1., z: dy_z}));
-				let NdotL = dot(n, xyz{x: 0., y: 0., z: 1.});
-				assert!(NdotL >= 0. && NdotL <= 1.);
-				vertices[(y*vertex_stride+x) as usize] = terrain::Vertex{
-					z: z(water[xy{x,y}]),
-					NdotL,
-					water: if water[xy{x,y}] > ground[xy{x,y}] { 1. } else { 0. } // Single terrain for water/ground (`water` has ground altitude for points without water)
-				};
-			}}
-		}
-		let mut cell_count = 0;
-		let center = size.signed()/2;
-		let radius2 = sq(min(size.x, size.y)/2);
-		for y in 0..size.y-2 { for x in 0..size.x-2 {
-			let r2 = vector::sq(xy{x,y}.signed()-center) as u32;
-			if r2 >= radius2 { continue; }
-			let i0 = y*vertex_stride+x;
-			if [0, 1, vertex_stride, vertex_stride+1].iter().all(|di| ground[(i0+di) as usize] > f32::MIN) {} else {continue;}
-			cell_count += 1;
-		}}
-		let grid = buffer(context, BufferUsage::INDEX_BUFFER, (cell_count*6) as usize)?;
-		{
-			let mut grid = grid.write()?;
-			let mut target = 0;
-			for y in 0..size.y-2 { for x in 0..size.x-2 {
-				let r2 = vector::sq(xy{x,y}.signed()-center) as u32;
-				if r2 >= radius2 { continue; }
-				let i0 = y*vertex_stride+x;
-				if [0, 1, vertex_stride, vertex_stride+1].iter().all(|di| ground[(i0+di) as usize] > f32::MIN) {} else {continue;}
-				grid[target+0] = i0;
-				grid[target+1] = i0+1;
-				grid[target+2] = i0+vertex_stride+1;
-				grid[target+3] = i0;
-				grid[target+4] = i0+vertex_stride+1;
-				grid[target+5] = i0+vertex_stride;
-				target += 6;
-			}}
-			assert!(target == grid.len());
-		}
+	pub fn new(context: &Context, _: &mut Commands, points: &[vec3]) -> Result<Self> {
+		let vertices = from_iter(context, BufferUsage::VERTEX_BUFFER, points.into_iter().map(|&v| terrain::Vertex{position: v.into()}))?;
 		Ok(Self{
-			pass: terrain::Pass::new(context, true)?,
-			vertex_grid_size_x,
-			grid,
+			pass: terrain::Pass::new(context, true, PrimitiveTopology::PointList)?,
 			vertices,
-			image: image(context, commands, color.as_ref())?
 		})
-	}*/
-	/*pub fn render(&self, context: &Context, commands: &mut Commands, color: Arc<ImageView>, depth: Arc<ImageView>, view_projection: mat4) -> Result {
-		let Self{pass, vertex_grid_size_x, grid, vertices, image} = self;
+	}
+	pub fn render(&self, context: &Context, commands: &mut Commands, color: Arc<ImageView>, depth: Arc<ImageView>, view_projection: mat4) -> Result {
+		let Self{pass, vertices} = self;
 		pass.begin_rendering(context, commands, color, Some(depth), true, &terrain::Uniforms{
 			view_projection: view_projection.map(|column| column.into()).into(), 
-			vertex_grid_size_x: *vertex_grid_size_x
-		}, &[
-			WriteDescriptorSet::image_view(1, ImageView::new_default(image.clone())?),
-			WriteDescriptorSet::sampler(2, linear(context)),
-		])?;
-		commands.bind_index_buffer(grid.clone())?;
+		}, &[])?;
 		commands.bind_vertex_buffers(0, vertices.clone())?;
-		unsafe{commands.draw_indexed(grid.len() as _, 1, 0, 0, 0)}?;
+		unsafe{commands.draw(vertices.len() as _, 0, 0, 0)}?;
 		commands.end_rendering()?;
 		Ok(())
-	}*/
+	}
 }
