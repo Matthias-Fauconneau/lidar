9d3e315d9ade063a73c2db86fb9011afaebead6d
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-04-16 12:56:50 +0200

    image

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ type Array<T=f32> = OwningRef<Box<memmap::Mmap>, [T]>;
 
 mod terrain; use terrain::Terrain;
 
-use vector::{xyz, MinMax, vec3, xy, size, int2, vec2, rotate, xyzw, vec4, mat4};
-use ui::vulkan::{Context, Commands, Arc, ImageView, Image, default, ImageCreateInfo, Format, ImageUsage};
+use {vector::{xyz, MinMax, vec3, xy, size, int2, vec2, rotate, xyzw, vec4, mat4}, image::{Image, rgb8}};
+use ui::vulkan::{Context, Commands, Arc, ImageView, Image as GPUImage, default, ImageCreateInfo, Format, ImageUsage};
 
 struct App {
 	terrain: Terrain,
@@ -28,8 +28,10 @@ fn new(context: &Context, commands: &mut Commands) -> Result<Self> {
 		let mut points = Vec::with_capacity(reader.header().number_of_points() as usize);
 		let las::Bounds{min, max} = reader.header().bounds();
 		let MinMax{min, max} = MinMax{min: {let las::Vector{x,y,z}=min; xyz{x,y,z}}, max: {let las::Vector{x,y,z}=max; xyz{x,y,z}}};
+		//println!("{min:?} {max:?}");
 		let center = (1./2.)*(min+max);
 	    let extent = max-min;
+		println!("{center:?} {extent:?}");
 	    let extent = extent.x.min(extent.y);
 	    
 		for point in reader.points() {
@@ -37,16 +39,40 @@ fn new(context: &Context, commands: &mut Commands) -> Result<Self> {
 			let x = 2.*(E-center.x)/extent;
 	        let y = 2.*(N-center.y)/extent;
 	        let z = 2.*(z-center.z)/extent;
-			//if x > -1./2. && x < 1./2. && y > -1./2. && y < 1./2. {
 			points.push(xyz{x,y,z}.into());
 		}
 		std::fs::write(cache, bytemuck::cast_slice::<vec3,_>(&points))?;
 		println!("{cache}");
 	}
 	let ref points = map::<vec3>(cache)?;
+	//let [min, max] = [xy{x: 2684000, y: 1248500}, xy{ x: 2684500, y: 1249000}];
+	let [center, extent] = [xy{x: 2684250, y: 1248750}, xy{ x: 500, y: 500}];
+	
+	let name = "2408";
+	let ref cache = format!("{}/.cache/lidar/{name}", std::env::var("HOME")?);
+	if !std::fs::exists(cache)? {
+		let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("{}/{name}.tif", std::env::var("HOME")?))?)?};
+	    let mut tiff = tiff::decoder::Decoder::new(std::io::Cursor::new(&*tiff))?;
+	    let (size_x, size_y) = tiff.dimensions()?;
+	    let size = size{x: size_x, y: size_y};
+		let [_, _, _, E, N, _] = tiff.get_tag_f64_vec(tiff::tags::Tag::ModelTiepointTag)?[..] else { panic!() };
+	    let [scale_E, scale_N, _] = tiff.get_tag_f64_vec(tiff::tags::Tag::ModelPixelScaleTag)?[..] else { panic!() };
+	    let min = xy{x: E, y: (N-scale_N*size.y as f64)};
+	    let max = xy{x: (E+scale_E*size.x as f64), y: N};
+	    println!("{min} {max}");
+		//println!("{:?}", tiff.image().chunk_offsets);
+		let image = Image::new(tiff.chunk_data_dimensions(0).into(), bytemuck::cast_vec(tiff.read_chunk(0)?));
+		//let image = load_rgb8(format!("{}/{name}.tif", std::env::var("HOME")?));
+		//println!("{}", image.size);
+		panic!();//image.slice(, 10000.into())
+		//std::fs::write(cache, bytemuck::cast_slice(image.data))?;
+		//println!("{cache}");
+	}
+	let image = map::<rgb8>(cache)?;
+	let image = image::Image::new(10000.into(), image);
 	Ok(Self{
-	terrain: Terrain::new(context, commands, points)?,
-	view_position: xy{x: 0., y: 0.}, yaw: 0.
+		terrain: Terrain::new(context, commands, points, image.as_ref())?,
+		view_position: xy{x: 0., y: 0.}, yaw: 0.
 	})
 }}
 
@@ -57,21 +83,21 @@ fn paint(&mut self, context@Context{memory_allocator, ..}: &Context, commands: &
 	let aspect_ratio = image_size.x as f32/image_size.y as f32;
 	
 	let view_projection = |xyz{x,y,z}:vec3| {
-	let xy{x,y} = rotate(*yaw, xy{x,y} - *view_position);
-	let xy{x: y, y: z} = rotate(-std::f32::consts::PI/3., xy{x: y, y: z});
-	let z = (z-1.)/2.;
-	let n = 1./4.;
-	let f = 2.;
-	if false { xyzw{x, y: aspect_ratio*y, z: -z, w: 1.}  } else {
-	let zz = -f/(f-n);
-	let z1 = -(f*n)/(f-n);
-	xyzw{x, y: aspect_ratio*y, z: zz*z+z1, w: -z}
-	}
+		let xy{x,y} = rotate(*yaw, xy{x,y} - *view_position);
+		let xy{x: y, y: z} = rotate(-std::f32::consts::PI/3., xy{x: y, y: z});
+		let z = (z-1.)/2.;
+		let n = 1./4.;
+		let f = 2.;
+		if false { xyzw{x, y: aspect_ratio*y, z: -z, w: 1.}  } else {
+			let zz = -f/(f-n);
+			let z1 = -(f*n)/(f-n);
+			xyzw{x, y: aspect_ratio*y, z: zz*z+z1, w: -z}
+		}
 	};
 	fn from_linear(linear: impl Fn(vec3)->vec4) -> mat4 {
-	let w = linear(xyz{x:0.,y:0.,z:0.});
-	let xyz{x,y,z} = xyz{x: xyz{x:1.,y:0.,z:0.}, y:xyz{x:0.,y:1.,z:0.}, z: xyz{x:0.,y:0.,z:1.}}.map(|e| linear(e)-w);
-	xyzw{x,y,z,w}
+		let w = linear(xyz{x:0.,y:0.,z:0.});
+		let xyz{x,y,z} = xyz{x: xyz{x:1.,y:0.,z:0.}, y:xyz{x:0.,y:1.,z:0.}, z: xyz{x:0.,y:0.,z:1.}}.map(|e| linear(e)-w);
+		xyzw{x,y,z,w}
 	}
 	let view_projection = from_linear(view_projection);
 	
--- src/terrain.rs
+++ src/terrain.rs
@@ -1,26 +1,31 @@
 use ui::{vulkan, shader};
 shader!{terrain}
-use vulkan::Subbuffer;
+use vulkan::{Arc, Subbuffer, Image as GPUImage};
 pub struct Terrain {
 	pass: terrain::Pass,
 	vertices: Subbuffer::<[terrain::Vertex]>,
+	image: Arc<GPUImage>,
 }
 
-use {ui::Result, vector::{vec3, mat4}};
-use vulkan::{Context, from_iter, BufferUsage, Commands, Arc, ImageView, PrimitiveTopology};
+use {ui::Result, vector::{vec3, mat4}, image::{Image, rgb8, rgba8}};
+use vulkan::{Context, from_iter, BufferUsage, Commands, ImageView, PrimitiveTopology, image, WriteDescriptorSet, linear};
 impl Terrain {
-	pub fn new(context: &Context, _: &mut Commands, points: &[vec3]) -> Result<Self> {
+	pub fn new(context: &Context, commands: &mut Commands, points: &[vec3], color: Image<&[rgb8]>) -> Result<Self> {
 		let vertices = from_iter(context, BufferUsage::VERTEX_BUFFER, points.into_iter().map(|&v| terrain::Vertex{position: v.into()}))?;
 		Ok(Self{
 			pass: terrain::Pass::new(context, true, PrimitiveTopology::PointList)?,
 			vertices,
+			image: image(context, commands, color.map(|&v| rgba8::from(v)).as_ref())?
 		})
 	}
 	pub fn render(&self, context: &Context, commands: &mut Commands, color: Arc<ImageView>, depth: Arc<ImageView>, view_projection: mat4) -> Result {
-		let Self{pass, vertices} = self;
+		let Self{pass, vertices, image} = self;
 		pass.begin_rendering(context, commands, color, Some(depth), true, &terrain::Uniforms{
 			view_projection: view_projection.map(|column| column.into()).into(), 
-		}, &[])?;
+		}, &[
+            WriteDescriptorSet::image_view(1, ImageView::new_default(image.clone())?),
+            WriteDescriptorSet::sampler(2, linear(context)),
+        ])?;
 		commands.bind_vertex_buffers(0, vertices.clone())?;
 		unsafe{commands.draw(vertices.len() as _, 1, 0, 0)}?;
 		commands.end_rendering()?;
